3215aeba82226855076e8d4b58ef1963f5461623
cmorales <caitlyn.morales424@gmail.com>
2024-09-11 14:34:45 -0400

    update

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,13 @@ public class Hello {
     public static void main(String[] args) {
 
         // first Java program!
-        System.out.println("new dawn, new day");
+    //    System.out.println("new dawn, new day");
+
+    //   double d = 2.9;
+    //   System.out.println((int)d);
+
+    int x = 5
+    int y = 2
+    System.out.println(x/y)
     }
 }
