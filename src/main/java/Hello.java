94472ce283b5a77dc04e0e85c3021d4b97cd773f
brandon mazey <brandon@brandons-Air.localdomain>
2024-09-23 13:44:39 -0400

    lab

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,21 @@
-public class Hello {
-    public static void main(String[] args) {
+import java.util.Random;
 
-        // first Java program!
-        System.out.println("new dawn, new day");
+public class Hello { 
+    public static void main(String[] args) { 
+        // Strings are complicated
+
+        String h = "Halloween";
+
+        Random random = new Random();
+
+        int r = random.nextInt(h.length());
+
+        // System.out.println("the random number is: " + r);
+        // System.out.println("the random letter is: " + h.charAt(r));
+
+        String s = "September";
+        String k = s.substring(0, 5);
+        System.out.println(k);
     }
 }
+// String e = new String(Character.toChars(0x1F43C));
