70c09eb70cd49da10b75aef3be0dd77aeade7233
Eleonor <eleonortiamartin@gmail.com>
2024-10-22 08:56:15 -0400

    combinator

--- src/main/java/factor/Combinator.java
+++ src/main/java/factor/Combinator.java
@@ -5,7 +5,11 @@ public class Combinator {
     // TODO document
     public static int factorial(int n) {
         // FIXME for +10 bonus!
-        return 0;
+        int product = 1;
+        for(int i = n; i >= 1; i--){
+            product = product*i;
+        }
+        return product;
     }
 
     // TODO document
