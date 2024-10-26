4f7e10b5f270ea4f87119adb48ecf867238040b0
erodrigues <erika.bklyn@gmail.com>
2024-10-25 21:07:20 -0400

    ionwan2go

--- src/main/java/factor/Combinator.java
+++ src/main/java/factor/Combinator.java
@@ -5,8 +5,10 @@ public class Combinator {
     // TODO document
     public static int factorial(int n) {
         int product = 1;
-        for (int i = n; i >= 1; i--){
-            product = product * 1;
+        // start at the number, and work your way back because the interval is -1
+        for (int i = n; i >= i; i--){
+            // Multiply the number by the number that holds the position before it
+            product = product * i;
         }
         // FIXME for +10 bonus!
         return product;
@@ -14,8 +16,13 @@ public class Combinator {
 
     // TODO document
     public static int choose(int n, int k) {
+        // this is the n! / k! * (n-k)! formula
+        // factorial was alraedy defined in the last problem
+        // so it just does whatever factorial does whenever theres a factorial to be done
+        return factorial(n) / (factorial(k) * factorial(n - k));
+
         // FIXME for +10 bonus!
-        return 0;
+        
     }
     
 }
--- src/main/java/factor/Fibonacci.java
+++ src/main/java/factor/Fibonacci.java
@@ -8,7 +8,25 @@ public class Fibonacci {
     // 0, 1, 1, 2, 3, 5, 8 ...
     // ex: n = 6 -> 8
     public static int sequence(int n) {
-        // TODO implement
-        return 0;
+        if (n == 0) return 0;  
+        if (n == 1) return 1;  
+    
+        int prev1 = 0;  
+        int prev2 = 1;  
+        int current = 0;
+    
+        // start at 2, go until less then or equal to n, increment is 1
+        for (int i = 2; i <= n; i++) {
+            // current number is the sum of the last two previous numbers
+            current = prev1 + prev2;  
+            // the previous number shits back
+            prev1 = prev2;  
+            // current number shifts back to become a previous number
+            prev2 = current;  
+        }
+    
+        return current;
     }
+        
 }
+
