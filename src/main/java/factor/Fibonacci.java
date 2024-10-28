5c2ba85a10bf76511060036371948663f557da94
Rachel McNamara <rmcnamara@stsaviour.org>
2024-10-28 07:43:02 -0400

    done! hopefully

--- src/main/java/factor/Fibonacci.java
+++ src/main/java/factor/Fibonacci.java
@@ -7,7 +7,16 @@ public class Fibonacci {
     // Recall the fibonacci sequence is defined as follows ...
     // 0, 1, 1, 2, 3, 5, 8 ...
     // ex: n = 6 -> 8
-    public static int sequence(int n) {
+    public static int fibonacci (int n) {
+        if (n == 1){
+            return  1;
+        }
+        if (n == 0) {
+            return 0;
+        }
+        return fibonacci (n-1) + fibonacci (n-2);
+    }
+        
         // TODO implement
         return 0;
     }
