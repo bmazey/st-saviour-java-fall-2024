9abea429624216edd605c7080bded063d0bd1100
Maricar Erlano <merlano@stsaviour.org>
2024-10-30 21:27:20 -0400

    bonus test done

--- src/main/java/factor/Fibonacci.java
+++ src/main/java/factor/Fibonacci.java
@@ -11,23 +11,20 @@ public class Fibonacci {
         // TODO implement
 
         // declares first and second position as 0 and 1
-        if(n<=1){
+        if(n <= 1){
             return n;
         }
-       
+        
         int[] result = new int[n + 1];
         result [0] = 0;
         result [1] = 1;
-       
-        // adds the two numbers before position i of result array for the value of the nth position
-        for (int i = 2; i <= n; i++){
-            
-            result[i] = result[i-1] + result[i -2]; 
 
+        // adds the two numbers before position i of result array for the value of the nth position
+        for (int i = 2; i <= n; i++){           
+            result[i] = result[i-1] + result[i -2]; 
         }
         
         return result[n];
         
-
     }
 }
