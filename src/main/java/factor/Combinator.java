bc2a69cde31614174a93ab02e95ff1e0570c34ef
Eleonor <eleonortiamartin@gmail.com>
2024-10-30 21:06:12 -0400

    done

--- src/main/java/factor/Combinator.java
+++ src/main/java/factor/Combinator.java
@@ -16,25 +16,9 @@ public class Combinator {
 
     // TODO document
     public static int choose(int n, int k) {
-        int productn = 1;
+        // The choose formula 
+        return factorial(n) / (factorial(k) * factorial(n - k));
         
-        // Create recursive loop to compute n!
-        for(int i = n; i >= 1; i--){
-            productn = productn*i;
-        }
-
-        int productk = 1;
-        
-        // Create recursive loop to compute k!
-        for(int i = k; i >= 1; i--){
-            productk = productk * i;
-        }
-
-        // Implement given formula 
-        int denominator = productn - productk;
-        int result = productn / ( productk * denominator ); 
-
-        return result;
     }
     
 }
