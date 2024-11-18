3660d27972e3fd192d92b0a404cde33a58fd39ee
erodrigues <erika.bklyn@gmail.com>
2024-11-18 08:55:14 -0500

    cleaning up

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
+        Platypus Perry = new Platypus();
+        System.out.println(Perry.aquatic);
+        System.out.println(Perry.legs);
+        Perry.swim();
 
+
+        
         
     }
 }
